d22a9fc86d722da51e1ece0fb79ab9b964b311fb
Md. Abdul Mazed Rupok <mazed4g@gmail.com>
2016-05-11 22:27:54 +0600

    Create round-2 problem B.cpp

---
+++ CodeMask-2016/round-2 problem B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tc,a,b, k = 0, c;
+    //cout << 2%1 << endl;
+    scanf (" %d", &tc);
+    while (tc--) {
+        scanf (" %d %d", &a, &b);
+        if (b == 1) printf ("No\n");
+        else printf ("Yes\n");
+    }
+    return 0;
+}
