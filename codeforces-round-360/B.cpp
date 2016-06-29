f8dedd44f162608939f820fd20fe3d207d632d63
Md. Abdul Mazed Rupok <mazed4g@gmail.com>
2016-06-30 02:29:23 +0600

    Create B.cpp

---
+++ codeforces-round-360/B.cpp
@@ -0,0 +1,35 @@
+
+/**
+   MMM   MMM     A     ZZZZZZZ EEEEEEE DDDDD
+   MM MMM MM    AAA    Z   ZZ  EE    E DDDDDDD
+   MM  M  MM   AA AA      ZZ   EEEE    DD   DD
+   MM     MM  AAAAAAA   ZZ   Z EE    E DDDDDDD
+   MM     MM AA     AA ZZZZZZZ EEEEEEE DDDDD
+
+ Name:     Md. Abdul Mazed (RUPOK)
+           Noakhali Science & Technology University
+ Email:    mazedrupok@gmail.com || mazed4g@gmail.com
+ FB id:    www.facebook.com/mazedrupok
+ github:   www.github.com/mazedrupok
+ problem:
+ Catagory:
+*/
+#include <bits//stdc++.h>
+using namespace std;
+#define MX 100005
+#define read(f)  freopen(f,"r",stdin)
+#define write(f) freopen(f,"w",stdout)
+#define LL long long
+#define ULL unsigned long long
+
+int main ()
+{
+    //ios::sync_with_stdio(0); cin.tie(0);
+    string s;
+    cin >> s;
+    cout << s;
+    reverse (s.begin(), s.end());
+    cout << s << endl;
+
+    return 0;
+}
